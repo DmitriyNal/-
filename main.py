47d2734335ed9f2eb92e5dedd7b6aabe640ad422
DmitriyNal <dima7kiper@yandex.ru>
2024-10-29 10:54:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+
+class MyClass:
+    def __init__(self, value):
+        self.value = value
+
+    def print_value(self):
+        print(self.value)
+
+my_object = MyClass(42)
+
+def introspection_info(obj):
+    info = {}
+    #получение типа объекта
+    info['type'] = type(obj)
+
+    # Получение имени типа объекта
+    info['type_name'] = type(obj).__name__
+
+    # Получение атрибутов объекта
+    info['attributes'] = vars(obj)
+
+    # Получение методов объекта
+    info['methods'] = dir(obj)
+
+    # Получение модуля, к которому объект принадлежит
+    info['module'] = obj.__module__
+
+    return info
+
+if __name__ == '__main__':
+    number_info = introspection_info(my_object)
+    print(number_info)
+
+
+
+
+
